fae439e8bb04bf174e78a814a97abd177e4b8a11
Lucasgarcc <luuccasgarciia@gmail.com>
2024-05-07 19:58:51 -0300

    API Node com integração ao Postgresql

---
+++ db.js
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import postgres from "postgres";
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+ export const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
+
+async function getPgVersion() {
+  const result = await sql`select version()`;
+  console.log(' conecção ok', result);
+}
+
+getPgVersion();
---
+++ server.js
@@ -0,0 +1,54 @@
+import { fastify } from "fastify";
+// import { DatabaseMemory } from "./database-memory.js";
+import { DatabasePostgres } from "./database-postgres.js";
+
+const server = fastify();
+
+// const database = new DatabaseMemory();
+const database = new DatabasePostgres();
+
+server.post("/videos", async (request, reply) => {
+  const { title, description, duration } = request.body;
+
+  await database.create({
+    title,
+    description,
+    duration,
+  });
+
+  return reply.status(201).send();
+});
+
+server.get("/videos", async (request) => {
+  const search = request.query.search;
+
+  const videos = await database.list(search);
+
+  return videos;
+});
+
+server.put("/videos/:id", async (request, reply) => {
+  const videoId = request.params.id;
+  const { title, description, duration } = request.body;
+
+  await database.update(videoId, {
+    title,
+    description,
+    duration,
+  });
+
+  return reply.status(204).send();
+});
+
+server.delete("/videos/:id", async (request, reply) => {
+  const videoId = request.params.id;
+
+  await database.delete(videoId);
+
+  return reply.status(204).send();
+});
+
+server.listen({
+  host: "0.0.0.0",
+  port: process.env.PORT ?? 3333,
+});
